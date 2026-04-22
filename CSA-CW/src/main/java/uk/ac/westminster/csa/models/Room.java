2ded0caab5310f74bb28bcf31a085e7109c2f91d
Rism583 <risme1522@gmail.com>
2026-04-22 09:34:08 +0530

    implemented encapsulated sensor and sensorreadng POJO models

--- CSA-CW/src/main/java/uk/ac/westminster/csa/models/Room.java
+++ CSA-CW/src/main/java/uk/ac/westminster/csa/models/Room.java
@@ -17,6 +17,10 @@ public class Room {
     
     private List<String> sensorIds = new ArrayList<>();
 
+    public Room() {
+    }
+      
+
     public Room(String id, String name, int capacity) {
         this.id = id;
         this.name = name;
